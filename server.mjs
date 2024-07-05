96c06d8f27b7463091f85ab7def4cb4896628212
Tiago Pace <tiagopace.dev@gmail.com>
2024-07-05 17:32:18 -0300

    03 - Basic Server for Push Notifications

---
+++ server.mjs
@@ -0,0 +1,40 @@
+import express from 'express'
+import webpush from 'web-push'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+let subscriptionData = null
+
+webpush.setVapidDetails(
+  `mailto:${process.env.VAPID_MAILTO}`,
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+)
+
+app.get('/send-notification', async (req, res) => {
+  try {
+    await webpush.sendNotification(
+      subscriptionData,
+      JSON.stringify({
+        title: 'Hello World',
+        body: 'Notification sent from backend',
+      })
+    )
+    res.sendStatus(200)
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500)
+  }
+})
+
+app.post('/save-subscription', async (req, res) => {
+  subscriptionData = req.body
+  res.sendStatus(200)
+})
+
+app.use(express.static('./public'))
+app.listen(8000)
